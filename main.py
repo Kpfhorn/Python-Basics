2219c8e828c3777d3b44074b1f42d6ccdfbb3b4b
Kevin Babcock <kpfhorn@gmail.com>
2020-12-04 06:02:21 +0000

    fix string split error on line 123

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ for i in l:
 ###Strings in Python are immutable. this means that they cannot be modified once created. To alter strings, you must assemble a new string using data from the old string
 
 message = 'hello world'
-string_list = message.split("")
+string_list = list(message)
 string_list[4] = ''
 message2 = "".join(string_list)
 print(message)
