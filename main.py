c23ae9cf0ce56a6b64eed2734df5dfa2c70a2512
Kevin Babcock <kpfhorn@gmail.com>
2020-12-04 05:46:06 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -116,3 +116,12 @@ for i in l:
 
 
 
+#Modifying Python Strings
+###Strings in Python are immutable. this means that they cannot be modified once created. To alter strings, you must assemble a new string using data from the old string
+
+message = 'hello world'
+string_list = message.split("")
+string_list[4] = ''
+message2 = "".join(string_list)
+print(message)
+print(message2)
